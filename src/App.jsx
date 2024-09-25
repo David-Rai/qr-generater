021446df840ec8e487ca11a743f31ac0f3b9d590
david-rai <davidkhaling123@.com>
2024-09-25 21:07:42 +0545

    version 1.0

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { useEffect, useState } from "react";
+
+const App = () => {
+  const [input, setInput] = useState("");
+  const [url, setUrl] = useState("");
+
+  useEffect(() => {
+    async function Get() {
+      const response = await fetch(
+        `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${input}`
+      );
+      if (response.ok) {
+        setUrl(response.url);
+      }
+    }
+    Get();
+  }, [input]);
+
+  return (
+    <div className="app">
+      <div className="center">
+        {url ? (
+          <img src={url} alt="" />
+        ) : (
+          <img src="https://qrcode.co.uk/themes/altum/assets/images/qr_code.svg"></img>
+        )}
+        <input
+          type="text"
+          placeholder="Enter name or url"
+          onChange={(e) => setInput(e.target.value)}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default App;
